837f30d5a4d66fecd498115f0c44732c74856888
339463846 <339463846@2260I-L29-006>
2024-09-05 09:34:17 -0400

    Add project files.

---
+++ Topic 1 - The Basics (Electric Boogaloo)/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Topic_1___The_Basics__Electric_Boogaloo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+            string firstName = "Christian";
+            string favMovie = "The Matrix";
+
+            Console.WriteLine($"Hello user, my name is {firstName}, and my favorite movie is {favMovie}.".ToLower());
+
+            favMovie = favMovie.ToUpper();
+
+            Console.WriteLine("Part 1");
+            Console.WriteLine();
+            Console.WriteLine($"{favMovie}");
+            Console.WriteLine();
+            Console.WriteLine(favMovie.Contains("THE"));
+            Console.WriteLine();
+
+            favMovie = favMovie.Replace("A", "@").Replace("E", "3");
+
+            Console.WriteLine(favMovie);
+            Console.WriteLine();
+
+            // Part 2
+            string quote = "Never gonna give you up. Never gonna let you down. Never gonna run around and desert you.";
+
+            Console.WriteLine("Part 2");
+            Console.WriteLine($"'{quote}' without any vowels (including y) is:");
+
+            quote = quote.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "").Replace("y", "");
+
+            Console.WriteLine(quote);
+            Console.WriteLine("(Yes, you did just get rick rolled)");
+            Console.WriteLine();
+
+            //Part 3
+            Console.WriteLine("Part 3");
+            Console.WriteLine();
+            Console.WriteLine("                   ,,,, ");
+            Console.WriteLine("             ,;) .';;;;',");
+            Console.WriteLine(" ;;,,_,-.-.,;;'_,|I\\;;;/),,_");
+            Console.WriteLine("  `';;/:|:);{ ;;;|| \\;/ /;;;\\__");
+            Console.WriteLine("      L;/-';/ \\;;\\',/;\\/;;;.') \\");
+            Console.WriteLine("      .:`''` - \\;;'.__/;;;/  . _'-._ ");
+            Console.WriteLine("    .'/   \\     \\;;;;;;/.'_7:.  '). \\_");
+            Console.WriteLine("  .''/     | '._ );}{;//.'    '-:  '.,L");
+            Console.WriteLine(".'. /       \\  ( |;;;/_/         \\._./;\\   _,");
+            Console.WriteLine(" . /        |\\ ( /;;/_/             ';;;\\,;;_,");
+            Console.WriteLine(". /         )__(/;;/_/                (;;'''''");
+            Console.WriteLine(" /        _;:':;;;;:';-._             );");
+            Console.WriteLine("/        /   \\  `'`   --.'-._         \\/");
+            Console.WriteLine("       .'     '.  ,'         '-,");
+            Console.WriteLine("      /    /   r--,..__       '.\\");
+            Console.WriteLine("    .'    '  .'        '--._     ]");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+}
